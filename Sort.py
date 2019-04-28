d7c6ff3f65eb0039749d8aa874354a58ad3a2ffe
SEUNGWOO HAN <ashghan@naver.com>
2019-04-28 19:55:08 +0900

    Update Sort.py

--- Sort.py
+++ Sort.py
@@ -35,3 +35,24 @@ def ins_sort(a):
 d = [10,4,5,6,1,2,3,8,9,7]
 ins_sort(d)
 print(d)
+
+
+# 퀵 정렬
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    lesser_arr, equal_arr, greater_arr = [], [], []
+    for num in arr:
+        if num < pivot:
+            lesser_arr.append(num)
+        elif num > pivot:
+            greater_arr.append(num)
+        else:
+            equal_arr.append(num)
+    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+
+
+print(quick_sort([3,4,5,7,8,1,2]))
